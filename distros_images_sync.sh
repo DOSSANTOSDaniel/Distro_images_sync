6346e3826b9001a1a9f1321fbaf27819672b13a7
DOS SANTOS <dossantosjdf@gmail.com>
2024-11-04 16:34:02 +0100

    Update distros_images_sync.sh
    Update regex and add truenas.

--- distros_images_sync.sh
+++ distros_images_sync.sh
@@ -5,8 +5,8 @@
 #
 # Author:  'dossantosjdf@gmail.com'
 #
-# Date: 30/10/2024
-# Version: 3
+# Date: 29/10/2024
+# Version: 3.0
 # Bash_Version: 5.1.16
 #--------------------------------------------------#
 # Description:
@@ -50,64 +50,64 @@ aria_max_download_limit='13m'
 # --max-tries : Limite le nombre de tentatives en cas d'échec.
 aria_max_tries='3'
 # --retry-wait : Attente en secondes entre chaque nouvelle tentative.
-aria_retry_wait='10' 
+aria_retry_wait='10'
 
 # Dépendances nécessaire au bon fonctionnement du script
 dependencies=('curl' 'aria2c' 'wget' 'cksum')
 
 # URLs des différentes images systèmes a télécharger
 # Rocky Linux
-rocky_ver="$(curl -sL https://download.rockylinux.org/pub/rocky | grep -oP '(?<=href=")[0-9]*\.*[0-9]*\.*[0-9]*(?=/">)' | sort -Vr | head -n 1)"
+rocky_ver="$(curl -sL https://download.rockylinux.org/pub/rocky | grep -oP '(?<=href=")[0-9]+(\.?[0-9]*)*(?=/">)' | sort -Vr | head -n 1)"
 rocky_url="https://download.rockylinux.org/pub/rocky/${rocky_ver}/isos/x86_64/Rocky-${rocky_ver}-x86_64-dvd.torrent"
 
 # Alma Linux
-alma_ver="$(curl -sL https://repo.almalinux.org/almalinux/ | grep -oP '(?<=href=")[0-9]*\.*[0-9]*\.*[0-9]*(?=/">)' | sort -Vr | head -n 1)"
+alma_ver="$(curl -sL https://repo.almalinux.org/almalinux/ | grep -oP '(?<=href=")[0-9]+(\.?[0-9]*)*(?=/">)' | sort -Vr | head -n 1)"
 alma_url="https://raw.repo.almalinux.org/almalinux/${alma_ver}/isos/x86_64/AlmaLinux-${alma_ver}-x86_64.torrent"
 
 # Fedora
-fedora_ver="$(curl -sL https://torrent.fedoraproject.org/torrents/ | grep -oP '(?<=href=")Fedora-Workstation-Live-x86_64-[0-9]*\.*[0-9]*\.*[0-9]*\.torrent(?=">)' | sort -Vr | head -n 1)"
+fedora_ver="$(curl -sL https://torrent.fedoraproject.org/torrents/ | grep -oP '(?<=href=")Fedora-Workstation-Live-x86_64-[0-9]+(\.?[0-9]*)*\.torrent(?=">)' | sort -Vr | head -n 1)"
 fedora_url="https://torrent.fedoraproject.org/torrents/${fedora_ver}"
 
 # FreeBSD
-freebsd_ver="$(curl -sL https://download.freebsd.org/releases/ISO-IMAGES/ | grep -oP '(?<=href=")[0-9]*\.*[0-9]*\.*[0-9]*(?=/" )' | sort -Vr | head -1)"
+freebsd_ver="$(curl -sL https://www.freebsd.org/ | grep -A 1 'Production:' | tr -d '\n' | grep -oP 'releases/\K[0-9]+(\.?[0-9]*)*')"
 freebsd_url="https://download.freebsd.org/releases/ISO-IMAGES/${freebsd_ver}/FreeBSD-${freebsd_ver}-RELEASE-amd64-dvd1.iso"
 
 # Ubuntu Server
-ubuntu_ver="$(curl -sL https://releases.ubuntu.com/ | grep -oP '(?<=href=")[0-9]*\.04\.*[0-9]*(?=/">)' | sort -Vr | head -n 1)"
+ubuntu_ver="$(curl -sL https://releases.ubuntu.com/ | grep -oP '(?<=href=")[0-9]+\.04\.?[0-9]*(?=/">)' | sort -Vr | head -n 1)"
 ubuntusrv_url="https://releases.ubuntu.com/${ubuntu_ver}/ubuntu-${ubuntu_ver}-live-server-amd64.iso.torrent"
 
 # Ubuntu
 ubuntu_url="https://releases.ubuntu.com/${ubuntu_ver}/ubuntu-${ubuntu_ver}-desktop-amd64.iso.torrent"
 
 # OpenSUSE
-opensuse_ver="$(curl -sL https://get.opensuse.org/leap | grep -oP '(?<=href="/leap/)[0-9]*\.*[0-9]*\.*[0-9]*(?=/">)')"
+opensuse_ver="$(curl -sL https://get.opensuse.org/leap | grep -oP '(?<=href="/leap/)[0-9]+(\.?[0-9]*)*(?=/">)')"
 opensuse_url="https://download.opensuse.org/distribution/leap/${opensuse_ver}/iso/openSUSE-Leap-${opensuse_ver}-DVD-x86_64-Current.iso"
 
 # Proxmox
-proxmox_ver="$(curl -sL https://enterprise.proxmox.com/iso/ | grep -oP '(?<=href=")proxmox-ve_[0-9]*\.*[0-9]*\.*[0-9]*-[0-9]*\.iso(?=">)' | sort -Vr | head -1)"
+proxmox_ver="$(curl -sL https://enterprise.proxmox.com/iso/ | grep -oP '(?<=href=")proxmox-ve_[0-9]+(\.?[0-9]*)*-[0-9]*\.iso(?=">)' | sort -Vr | head -1)"
 proxmox_url="https://enterprise.proxmox.com/iso/${proxmox_ver}"
 
 # LinuxMint
-mint_ver="$(curl -sL https://www.linuxmint.com/torrents/ | grep -oP '(?<=href=")linuxmint-[0-9]*\.*[0-9]*\.*[0-9]*-cinnamon-64bit.iso.torrent(?=">)' | sort -Vr | head -n 1)"
+mint_ver="$(curl -sL https://www.linuxmint.com/torrents/ | grep -oP '(?<=href=")linuxmint-[0-9]+(\.?[0-9]*)*-cinnamon-64bit.iso.torrent(?=">)' | sort -Vr | head -n 1)"
 mint_url="https://www.linuxmint.com/torrents/$mint_ver"
 
 # Debian
-debian_ver="$(curl -sL https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/ | grep -oP '(?<=href=")debian-[0-9]*\.*[0-9]*\.*[0-9]*-amd64-DVD-1\.iso\.torrent(?=">)' | sort -Vr | head -n 1)"
+debian_ver="$(curl -sL https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/ | grep -oP '(?<=href=")debian-[0-9]+(\.?[0-9]*)*-amd64-DVD-1\.iso\.torrent(?=">)' | sort -Vr | head -n 1)"
 debian_url="https://cdimage.debian.org/debian-cd/current/amd64/bt-dvd/${debian_ver}"
 
 # ArchLinux
 arch_url="https://archlinux.mirrors.ovh.net/archlinux/iso/latest/archlinux-x86_64.iso"
 
 # Kali Linux
-kali_ver="$(curl -sL https://cdimage.kali.org/kali-images/current/ | grep -oP '(?<=href=")kali-linux-[0-9]*\.*[0-9]*\.*[0-9]*-live-amd64\.iso\.torrent(?=")' | sort -rV | head -n 1)"
+kali_ver="$(curl -sL https://cdimage.kali.org/kali-images/current/ | grep -oP '(?<=href=")kali-linux-[0-9]+(\.?[0-9]*)*-live-amd64\.iso\.torrent(?=")' | sort -rV | head -n 1)"
 kali_url="https://cdimage.kali.org/kali-images/current/${kali_ver}"
 
 # Kali everything
-kali_e_ver="$(curl -sL https://cdimage.kali.org/kali-images/current/ | grep -oP '(?<=href=")kali-linux-[0-9]*\.*[0-9]*\.*[0-9]*-live-everything-amd64.iso.torrent(?=")' | sort -rV | head -n 1)"
+kali_e_ver="$(curl -sL https://cdimage.kali.org/kali-images/current/ | grep -oP '(?<=href=")kali-linux-[0-9]+(\.?[0-9]*)*-live-everything-amd64\.iso\.torrent(?=")' | sort -rV | head -n 1)"
 kali_e_url="https://cdimage.kali.org/kali-images/current/${kali_e_ver}"
 
 # Alpine
-alpine_ver="$(curl -sL https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/ | grep -oP '(?<=href=")alpine-extended-[0-9]*\.*[0-9]*\.*[0-9]*-x86_64\.iso(?=">)' | sort -rV | head -n 1)"
+alpine_ver="$(curl -sL https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/ | grep -oP '(?<=href=")alpine-extended-[0-9]+(\.?[0-9]*)*-x86_64\.iso(?=">)' | sort -rV | head -n 1)"
 alpine_url="https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/${alpine_ver}"
 
 # Netboot.xyz
@@ -129,7 +129,7 @@ ikki_ver="$(curl -sL -I -o /dev/null -w '%{url_effective}' 'https://sourceforge.
 ikki_url="${ikki_ver%%\?*}"
 
 # Ploplinux
-plop_ver="$(curl -sL https://download.plop.at/ploplinux/ | grep -oP '(?<=href=")[0-9]*\.*[0-9]*\.*[0-9]*(?=/">)' | sort -r | head -n 1)"
+plop_ver="$(curl -sL https://download.plop.at/ploplinux/ | grep -oP '(?<=href=")[0-9]+(\.?[0-9]*)*(?=/">)' | sort -r | head -n 1)"
 plop_url="https://download.plop.at/ploplinux/$plop_ver/live/ploplinux-${plop_ver}-x86_64.iso"
 
 # Redorescue
@@ -145,14 +145,14 @@ systemr_ver="$(curl -sL -I -o /dev/null -w '%{url_effective}' 'https://sourcefor
 systemr_url="${systemr_ver%%\?*}"
 
 # Tails
-tails_ver="$(curl -sL https://tails.net/torrents/files/ | grep -oP '(?<=href=")tails-amd64-[0-9]*\.*[0-9]*\.*[0-9]*\.iso\.torrent(?=")')"
+tails_ver="$(curl -sL https://tails.net/torrents/files/ | grep -oP '(?<=href=")tails-amd64-[0-9]+(\.?[0-9]*)*\.iso\.torrent(?=")')"
 tails_url="https://tails.net/torrents/files/$tails_ver"
 
 # Ipfire
 ipfire_url="$(curl -sL https://downloads.ipfire.org | grep -oP '(?<=href=")[a-z]*://.*x86_64\.iso')"
 
 # Opnsense
-opnsense_ver="$(curl -sL https://mirror.ams1.nl.leaseweb.net/opnsense/releases/ | grep -oP '(?<=href=")[0-9]*\.*[0-9]*\.*[0-9]*(?=/" )' | sort -Vr | head -n 1)"
+opnsense_ver="$(curl -sL https://mirror.ams1.nl.leaseweb.net/opnsense/releases/ | grep -oP '(?<=href=")[0-9]+(\.?[0-9]*)*(?=/" )' | sort -Vr | head -n 1)"
 opnsense_url="https://mirror.ams1.nl.leaseweb.net/opnsense/releases/$opnsense_ver/OPNsense-${opnsense_ver}-dvd-amd64.iso.bz2"
 
 # Q4OS
@@ -160,8 +160,11 @@ q4os_ver="$(curl -sL -I -o /dev/null -w '%{url_effective}' 'https://sourceforge.
 q4os_url="${q4os_ver%%\?*}"
 
 # Medicat
-medicat_ver="$(curl -sL https://files.dog/OD%20Rips/MediCat/ | grep -oP '(?<=href="v)[0-9]*\.[0-9]*(?=/" )' | sort -Vr | head -n 1)"
+medicat_ver="$(curl -sL https://files.dog/OD%20Rips/MediCat/ | grep -oP '(?<=href="v)[0-9]+(\.?[0-9]*)*(?=/" )' | sort -Vr | head -n 1)"
 medicat_url="https://raw.githubusercontent.com/mon5termatt/medicat_installer/main/download/MediCat_USB_v${medicat_ver}.torrent"
+
+# TrueNas
+truenas_url="$(curl -sL https://www.truenas.com/download-truenas-scale/ | grep 'Download STABLE' | grep -oP 'href="\Khttp[s]?://[^"]+\.iso')"
 
 # Déclaration du tableau associatif pour stocker les expressions régulières et les URLs de chaque distribution
 declare -A distros
@@ -195,6 +198,7 @@ distros=(
   [opnsense]="$opnsense_url"
   [q4os]="$q4os_url"
   [medicat]="$medicat_url"
+  [truenas]="$truenas_url"
 )
 
 # Fonctions
@@ -210,8 +214,7 @@ if [ ! -d "$images_dir" ]; then
     mkdir -p "$images_dir"
 fi
 
-# Bannière pour le fichier de journalisation
-message_log '---------'
+# Bannière fichier log
 message_log '-----------'
 message_log "-------------- Récupération images systèmes : $log_date -------"
 
@@ -254,7 +257,7 @@ for distro in "${!distros[@]}"; do
         # --retry-wait : Attente en secondes entre chaque nouvelle tentative.
         # --max-download-limit : Limite la vitesse de téléchargement.
         # --check-integrity : Vérifie l'intégrité du fichier, s'il existe déjà, aria2 ne le télécharge pas.
-	# --save-session : Sauvegarde la session actuelle, permet de reprendre un téléchargement interrompu.
+        # --save-session : Sauvegarde la session actuelle utile pour reprendre plus tard si besoin.
         aria2c --dir="$images_dir" \
           --continue=true \
           --seed-time=0 \
@@ -262,7 +265,7 @@ for distro in "${!distros[@]}"; do
           --retry-wait="$aria_retry_wait" \
           --max-download-limit="$aria_max_download_limit" \
           --check-integrity=true \
-	  --save-session="$images_dir/.aria_session" \
+          --save-session="$images_dir/.aria_session" \
           --quiet \
           "${image_url_part%/}/$image_file_part"
 
@@ -338,19 +341,19 @@ if [ -n "$(find "$images_dir" -mindepth 1 -print -quit)" ]; then
       -iname "*.7z" \) -exec cksum {} \; > "$final_sum"
 
   if [ -f "$init_sum" ]; then
-    # Comparer les deux fichiers d'empreintes et récupérer les différences(nouvelles images téléchargées)
+        # Comparer les deux fichiers d'empreintes et récupérer les différences(nouvelles images téléchargées)
     mapfile -t diff_output < <(diff "$init_sum" "$final_sum" | grep "^>" | cut -d' ' -f4-)
     if [ -n "${diff_output[*]}" ]; then
-      # Comparer chaque nouvelle image(check_sum_line) avec la liste des anciennes images(line)
+          # Comparer chaque nouvelle image(check_sum_line) avec la liste des anciennes images(line)
       for check_sum_line in "${diff_output[@]}"; do
         message_log "Mise à jour: $(basename "$check_sum_line")"
-        new_file="$(echo "$check_sum_line" | tr -cd '[:alpha:]')" 
-	# Lire le fichier init_sum(ancienne sommme de contrôle) ligne par ligne et comparer avec les noms des nouvelles images
+        new_file="$(echo "$check_sum_line" | tr -cd '[:alpha:]')"
+                # Lire le fichier init_sum(ancienne sommme de contrôle) ligne par ligne et comparer avec les noms des nouvelles images
         while IFS= read -r line; do
           old_file="$(echo "$line" | tr -cd '[:alpha:]')"
-	  # Teste si les deux fichiers sont strictement différents
+                  # Teste si les deux fichiers sont strictement différents
           if [ "$check_sum_line" != "$line" ]; then
-	    # Teste si les fichiers ont le même nom mais ont une version ancienne
+                        # Teste si les fichiers ont le même nom mais ont une version ancienne
             if [ "$new_file" = "$old_file" ]; then
               # Supprimer aussi le dossier si le fichier est dans un dossier
               chemin_dossier="$(dirname "$line")"
